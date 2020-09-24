3e77897ab9bc0551e2edac47179a5172a7c4b446
roman <roman.predein@apiumtech.com>
2020-09-24 17:15:29 +0200

    init

--- src/app/sagas.ts
+++
@@ -1,8 +0,0 @@
-import {all, fork} from 'redux-saga/effects';
-import userSaga from "./user/user.saga";
-
-export default function* rootSaga() {
-    yield all([
-        fork(userSaga)
-    ])
-};
--- src/common/A.component.tsx
+++
@@ -1,5 +0,0 @@
-import * as React from "react";
-
-export default function AComponent() {
-    return <div> this is component A</div>
-}
